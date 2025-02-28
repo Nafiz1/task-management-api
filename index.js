d26c68ae6f02c83e284299e1b2b1097d0330fdbe
Nafiz <nafizh007@gmail.com>
2025-02-28 00:56:11 -0500

    Implement CRUD operations and add MongoDB connection

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+// Import task routes
+const taskRoutes = require('./routes/taskRoutes');
+
 const app = express();
 const port = 3000;
 
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// MongoDB Connection
+mongoose.connect('mongodb://localhost:27017/taskmanager', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Routes
 app.get('/', (req, res) => {
   res.send('Task Management API');
 });
 
+// Use task routes
+app.use('/tasks', taskRoutes);
+
+// Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
